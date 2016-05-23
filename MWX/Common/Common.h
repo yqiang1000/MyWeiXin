6aeb2cd777b4fe115a4dd2978cde50d7ae6255d6
WeibaYeQiang <yqiang1000@163.com>
2016-05-23 15:13:55 +0800

    增加聊天测试

--- MWX/Common/Common.h
+++ MWX/Common/Common.h
@@ -157,3 +157,84 @@
 //        }
 //    }];
 //
+
+//- (void)addfirend:(NSString *)name {
+//    AVOSCloudManager *manager = [AVOSCloudManager defaultManager];
+//    
+//    [manager nameIsExist:name result:^(NSInteger *code, NSError *error) {
+//        
+//        if (!error) {
+//            if (code == 0) {
+//                NSLog(@"This friend already");
+//                
+//            } else {
+//                NSLog(@"正在添加。。。");
+//                [manager addFirend:name];
+//            }
+//        } else {
+//            NSLog(@"出错了:%@",error);
+//        }
+//        
+//    }];
+//}
+//
+//- (void)queryHave:(NSString *)name {
+//    
+//    AVUser *currentUser = [AVUser currentUser];
+//    AVRelation *relation = [currentUser relationforKey:@"firends"];
+//    
+//    AVQuery *query = [relation query];
+//    
+//    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+//        for (AVUser *firend in objects) {
+//            NSLog(@"firendID>>>%@",firend.objectId);
+//            NSLog(@"firendName>>>%@",firend.username);
+//            NSLog(@"firendTel>>>%@",firend.mobilePhoneNumber);
+//        }
+//    }];
+//    
+//}
+//
+//
+//- (void)addUser {
+//    
+//    NSArray *userName = @[
+//                          @"yeqiang",
+//                          @"pengxiuxiu",
+//                          @"yangxuguang",
+//                          @"yangyongzhao",
+//                          @"yangaichun",
+//                          @"zhangsan",
+//                          @"lisi",
+//                          @"wangwu",
+//                          @"xudonglin",
+//                          @"guoyueqi"
+//                          ];
+//    
+//    for (NSInteger i = 0; i < userName.count; i++) {
+//        AVUser *user = [[AVUser alloc] initWithClassName:@"_User"];
+//        user.username = userName[i];
+//        user.password = @"12345";
+//        user.mobilePhoneNumber = [NSString stringWithFormat:@"%ld",15355495537+i];
+//        [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+//            if (succeeded) {
+//                NSLog(@"%@添加成功",user.username);
+//            } else {
+//                NSLog(@"%@添加失败:%@",user.username,error);
+//            }
+//        }];
+//    }
+//    
+//    
+//}
+//
+//- (void)dosomething {
+//    
+//    __block NSArray *list = [[NSArray alloc] init];
+//    self.manager = [AVOSCloudManager defaultManager];
+//    [self.manager getAllFirends:^(NSArray *array, NSError *error) {
+//        list = array;
+//        NSLog(@"%@",list);
+//    }];
+//    
+//}
--- MWX/First/FirstViewController.m
+++ MWX/First/FirstViewController.m
@@ -9,11 +9,15 @@
 #import "FirstViewController.h"
 #import "Common.h"
 #import "AVOSCloudManager.h"
+#import <AVIMClient.h>
+#import <AVIMConversation.h>
+#import <AVIMTextMessage.h>
 
 
-@interface FirstViewController ()
+@interface FirstViewController ()<AVIMClientDelegate>
 
 @property (nonatomic, strong) AVOSCloudManager *manager;
+@property (nonatomic, strong) AVIMClient *client;
 
 @end
 
@@ -21,96 +25,45 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     [self userIsLogined];
-//    [self queryHave:nil];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setTitle:@"微信"];
     
-//    [self addfirend:@"123"];
+//    [self sendToPengxiuxiu];
+    [self receiveMessage];
 
 }
 
-- (void)addfirend:(NSString *)name {
-    AVOSCloudManager *manager = [AVOSCloudManager defaultManager];
-    
-    [manager nameIsExist:name result:^(NSInteger *code, NSError *error) {
-        
-        if (!error) {
-            if (code == 0) {
-                NSLog(@"This friend already");
-                
-            } else {
-                NSLog(@"正在添加。。。");
-                [manager addFirend:name];
-            }
-        } else {
-            NSLog(@"出错了:%@",error);
-        }
-        
-    }];
-}
 
-- (void)queryHave:(NSString *)name {
-    
-    AVUser *currentUser = [AVUser currentUser];
-    AVRelation *relation = [currentUser relationforKey:@"firends"];
-    
-    AVQuery *query = [relation query];
-    
-    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        for (AVUser *firend in objects) {
-            NSLog(@"firendID>>>%@",firend.objectId);
-            NSLog(@"firendName>>>%@",firend.username);
-            NSLog(@"firendTel>>>%@",firend.mobilePhoneNumber);
-        }
-    }];
-    
-}
-
-
-- (void)addUser {
-    
-    NSArray *userName = @[
-                          @"yeqiang",
-                          @"pengxiuxiu",
-                          @"yangxuguang",
-                          @"yangyongzhao",
-                          @"yangaichun",
-                          @"zhangsan",
-                          @"lisi",
-                          @"wangwu",
-                          @"xudonglin",
-                          @"guoyueqi"
-                           ];
-    
-    for (NSInteger i = 0; i < userName.count; i++) {
-        AVUser *user = [[AVUser alloc] initWithClassName:@"_User"];
-        user.username = userName[i];
-        user.password = @"12345";
-        user.mobilePhoneNumber = [NSString stringWithFormat:@"%ld",15355495537+i];
-        [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-            if (succeeded) {
-                NSLog(@"%@添加成功",user.username);
-            } else {
-                NSLog(@"%@添加失败:%@",user.username,error);
-            }
+- (void)sendToPengxiuxiu {
+    self.client = [[AVIMClient alloc] init];
+    [self.client openWithClientId:@"yeqiang" callback:^(BOOL succeeded, NSError *error) {
+        [self.client createConversationWithName:@"maoAndmouse" clientIds:@[@"xiuxiu"] callback:^(AVIMConversation *conversation, NSError *error) {
+            [conversation sendMessage:[AVIMTextMessage messageWithText:@"hello world" attributes:nil] callback:^(BOOL succeeded, NSError *error) {
+                if (succeeded) {
+                    NSLog(@"fasongchenggong");
+                } else {
+                    NSLog(@"fasongshibai:%@",error);
+                }
+            }];
         }];
-    }
-    
+    }];
     
 }
 
-- (void)dosomething {
-    
-    __block NSArray *list = [[NSArray alloc] init];
-    self.manager = [AVOSCloudManager defaultManager];
-    [self.manager getAllFirends:^(NSArray *array, NSError *error) {
-        list = array;
-        NSLog(@"%@",list);
+
+- (void)receiveMessage {
+    self.client = [[AVIMClient alloc] init];
+    _client.delegate = self;
+    [_client openWithClientId:@"xiuxiu" callback:^(BOOL succeeded, NSError *error) {
+        
     }];
-    
+}
+
+- (void)conversation:(AVIMConversation *)conversation didReceiveTypedMessage:(AVIMTypedMessage *)message {
+    NSLog(@"%@",message.text);
 }
 
 - (void)didReceiveMemoryWarning {
